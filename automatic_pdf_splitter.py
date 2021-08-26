857d2b294d6c70bc14ddf3625b6f8c5b2cfba32a
root <root@LAPTOP-I199DV5G.localdomain>
2021-08-26 19:25:58 -0300

    small fix

--- automatic_pdf_splitter.py
+++ automatic_pdf_splitter.py
@@ -21,13 +21,18 @@ def read_text_file(file_path):
   
 # Iterate through all files
 for file in os.listdir():
+
     # Check if file is in pdf format
     if file.endswith(".pdf"):
+
         file_path = f"{dirPath}/{path}/{file}"
+
+        file_name = os.path.splitext(file)[0]
+
         inputpdf = PdfFileReader(open(file_path , "rb"))
 
         for i in range(inputpdf.numPages):
             output = PdfFileWriter()
             output.addPage(inputpdf.getPage(i))
-            with open(dirPath+"/splitted/page_%s.pdf" % i, "wb") as outputStream:
+            with open(dirPath+"/splitted/%s_page_%s.pdf" % (file_name , i) , "wb") as outputStream:
                 output.write(outputStream)
